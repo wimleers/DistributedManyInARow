44b36dc460e18d2e1bf552d1d73048a95019b3db
Brecht Schoolmeesters <brecht123@gmail.com>
2009-11-06 22:15:00 +0000

    Changes to MainWindow to start a new game and add this in a new tab
    git-svn-id: svn://localhost/gs@23 bd4aa7da-f6f4-4997-a956-5ad611f6b780

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -10,6 +10,10 @@ class MainWindow(QtGui.QMainWindow):
         
         self.createLayout()
         self.createMenu()
+        self.showMaximized()
+        startDialog = StartDialog(self)
+        QtCore.QObject.connect(startDialog, QtCore.SIGNAL("startNewGame()"), self.createNewGame)
+        startDialog.exec_()
         
     def createLayout(self):
         self.gridLayout = QtGui.QGridLayout()
@@ -27,9 +31,11 @@ class MainWindow(QtGui.QMainWindow):
         self.menuBar().addMenu(gameMenu)
         
     def createNewGame(self):
-        #TODO: implement. Launch dialog asking info needed to start new game
-        x = 1
-        self.tabWidget.addTab(GameWidget(5, 7, "1000", self.tabWidget), "bleeh")
+        #TODO: implement. Start a new game using interface for network functionality
+        newGameDialog = NewGameDialog(self)
+        (gameName, gameComment, inARow, numRows, numCols) = newGameDialog.getGameInfo()
+        if(gameName != None):
+            self.tabWidget.addTab(GameWidget(numRows, numCols, self.tabWidget), gameName)
         
     def joinGame(self):
         #TODO: implement. Launch dialog asking which game to join
@@ -44,12 +50,90 @@ class StartDialog(QtGui.QDialog):
     def createLayout(self):
         self.layout = QtGui.QHBoxLayout(self)
         startNewGameButton = QtGui.QPushButton("Start a game", self)
+        startNewGameButton.clicked.connect(self.startNewGameClicked)
         joinGameButton = QtGui.QPushButton("Join a game", self)
-        
+        joinGameButton.clicked.connect(self.joinGameClicked)
         self.layout.addWidget(startNewGameButton)
         self.layout.addWidget(joinGameButton)
         
+    def startNewGameClicked(self):
+        self.emit(QtCore.SIGNAL("startNewGame()"))
+        self.accept()
+        
+    def joinGameClicked(self):
+        self.emit(QtCore.SIGNAL("joinGame()"))
+        self.accept()
+        
         #TODO: buttons moeten signal nog doorgeven aan mainwindow zodat correct venster geopend kan worden
+        
+class NewGameDialog(QtGui.QDialog):
+    def __init__(self, win_parent = None):
+        QtGui.QDialog.__init__(self, win_parent)
+        self.createLayout()
+        
+        self.gameName = ""
+        self.gameComment = ""
+        
+    def getGameInfo(self):
+        #can be called when we want to launch a new game. Returns the values needed to create a new game
+        self.exec_()
+        if(self.result() == 1):
+            #if the dialog was accepted (start game was clicked)
+            return (self.gameName, self.gameComment, self.inARow, self.numRows, self.numCols)
+        else:
+            return (None, None, None, None, None)
+        
+    def createLayout(self):
+        gridLayout = QtGui.QGridLayout(self)
+        label = QtGui.QLabel("New game parameters: ", self)
+        label2 = QtGui.QLabel("Name: ", self)
+        label3 = QtGui.QLabel("Comment: ", self)
+        label4 = QtGui.QLabel("# Rows: ", self)
+        label5 = QtGui.QLabel("# Cols: ", self)
+        label6 = QtGui.QLabel("# In a row: ", self)
+        self.gameEdit = QtGui.QLineEdit(self)
+        self.commentEdit = QtGui.QLineEdit(self)
+        self.numRowEdit = QtGui.QSpinBox(self)
+        self.numRowEdit.setMinimum(1)
+        self.numRowEdit.setMaximum(30)
+        self.numRowEdit.setValue(1)
+        self.numColEdit = QtGui.QSpinBox(self)
+        self.numColEdit.setMinimum(1)
+        self.numColEdit.setMaximum(30)
+        self.numColEdit.setValue(1)
+        self.inARowEdit = QtGui.QSpinBox(self)
+        self.inARowEdit.setMinimum(1)
+        self.inARowEdit.setMaximum(30)
+        self.inARowEdit.setValue(1)
+        startButton = QtGui.QPushButton("&Start", self)
+        startButton.clicked.connect(self.paramsSet)
+        cancelButton = QtGui.QPushButton("&Cancel", self)
+        cancelButton.clicked.connect(self.reject)
+        
+        gridLayout.addWidget(label, 0, 0, 1, 2)
+        gridLayout.addWidget(label2, 1, 0)
+        gridLayout.addWidget(label3, 2, 0)
+        gridLayout.addWidget(label4, 3, 0)
+        gridLayout.addWidget(label5, 4, 0)
+        gridLayout.addWidget(label6, 5, 0)
+        gridLayout.addWidget(self.gameEdit, 1, 1)
+        gridLayout.addWidget(self.commentEdit, 2, 1)
+        gridLayout.addWidget(self.numRowEdit, 3, 1)
+        gridLayout.addWidget(self.numColEdit, 4, 1)
+        gridLayout.addWidget(self.inARowEdit, 5, 1)
+        gridLayout.addWidget(startButton, 6, 0)
+        gridLayout.addWidget(cancelButton, 6, 1)
+        
+    def paramsSet(self):
+        self.gameName = self.gameEdit.text()
+        self.gameComment = self.gameEdit.text()
+        self.inARow = self.inARowEdit.value()
+        self.numRows = self.numRowEdit.value()
+        self.numCols = self.numColEdit.value()
+        if(self.gameName == "" or self.gameComment == ""):
+            QtGui.QMessageBox.warning(self, "Incomplete", "Not all values were set correctly.")
+        else:
+            self.accept()
         
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
--- src/NetworkWidget.py
+++ src/NetworkWidget.py
@@ -8,81 +8,28 @@ class NetworkWidget(QtGui.QDialog):
         QtGui.QDialog.__init__(self, win_parent)
         
         self.createLayout()
-        self.listenOnly = False
-        self.services = []
         
-        self.gameList.itemDoubleClicked.connect(self.loadGame)
-        
-        self.launchNeighborhoodDiscovery()
-        
-    def closeEvent(self, event):
-        self.neighborhood.kill()
-        
-    def launchNeighborhoodDiscovery(self):
-        # Initialize the neighborhood.
-        port = 4444
-        if self.listenOnly:
-            port = 4445
-        self.neighborhood = Neighborhood(serviceName = platform.node(),
-                         serviceType = '_manyinarow._tcp',
-                         protocolVersion = 1,
-                         port = port,
-                         serviceRegistrationCallback=self.serviceRegistrationCallback,
-                         serviceRegistrationErrorCallback=self.serviceRegistrationErrorCallback,
-                         peerServiceDiscoveryCallback=self.peerServiceDiscoveryCallback,
-                         peerServiceRemovalCallback=self.peerServiceRemovalCallback,
-                         peerServiceUpdateCallback=self.peerServiceUpdateCallback,
-                         peerMessageCallback=self.peerMessageCallback)
-        
-        self.neighborhood.start()
-        
+        self.gameList.itemDoubleClicked.connect(self.startGame)
+
     def createLayout(self):
         self.layout = QtGui.QGridLayout(self)
         self.gameList = QtGui.QListWidget(self)
         self.layout.addWidget(self.gameList, 1, 0)
         
-        label = QtGui.QLabel("The following games are active: ")
+        label = QtGui.QLabel("The following games were found: ")
         self.layout.addWidget(label, 0, 0)
         
-    def updateNetworkGames(self):
+    def updateNetworkGames(self, games):
         print "updating"
         self.gameList.clear()
-        for game in self.services:
-            self.gameList.addItem(game["serviceName"])
+        for game in games:
+            self.gameList.addItem(game)
             
-    def loadGame(self, item):
+    def startGame(self, item):
         #todo: add code to signal back-end to connect to the selected game (in item text)
         print item.text()
         self.accept()
         #self.emit(bleeeh)
-        
-        
-    #Callback functions for Neighbourhood service discovery:
-    def serviceRegistrationCallback(self, sdRef, flags, errorCode, name, regtype, domain):
-        print "SERVICE REGISTRATION CALLBACK FIRED, params: sdRef=%d, flags=%d, errorCode=%d, name=%s, regtype=%s, domain=%s" % (sdRef.fileno(), flags, errorCode, name, regtype, domain)
-
-    def serviceRegistrationErrorCallback(self, sdRef, flags, errorCode, errorMessage, name, regtype, domain):
-        print "SERVICE REGISTRATION ERROR CALLBACK FIRED, params: sdRef=%d, flags=%d, errorCode=%d, errorMessage=%s, name=%s, regtype=%s, domain=%s" % (sdRef, flags, errorCode, errorMessage, name, regtype, domain)
-
-    def peerServiceDiscoveryCallback(self, serviceName, interfaceIndex, fullname, hosttarget, ip, port):
-        self.services[(serviceName, interfaceIndex)] = {"serviceName": serviceName, "interfaceIndex": interfaceIndex, "fullname": fullname, "hosttarget": hosttarget, "ip": ip, "port": port}
-        print "SERVICE DISCOVERY CALLBACK FIRED, params: serviceName=%s, interfaceIndex=%d, fullname=%s, hosttarget=%s, ip=%s, port=%d" % (serviceName, interfaceIndex, fullname, hosttarget, ip, port)
-
-    def peerServiceRemovalCallback(self, serviceName, interfaceIndex):
-        self.services.remove((services, interfaceIndex))
-        print "SERVICE REMOVAL CALLBACK FIRED, params: serviceName=%s, interfaceIndex=%d" % (serviceName, interfaceIndex)
-
-    def peerServiceUpdateCallback(self, serviceName, interfaceIndex, fullname, hosttarget, ip, port):
-        print "SERVICE UPDATE CALLBACK FIRED, params: serviceName=%s, interfaceIndex=%d, fullname=%s, hosttarget=%s, ip=%s, port=%d" % (serviceName, interfaceIndex, fullname, hosttarget, ip, port)
-
-    def peerMessageCallback(self, serviceName, interfaceIndex, txtRecords, updated, deleted):
-        print "PEER MESSAGE CALLBACK FIRED", serviceName, interfaceIndex, txtRecords
-        print "\tupdated:"
-        for key in updated:
-            print "\t\t", key, txtRecords[key]
-        print "\tdeleted:"
-        for key in deleted:
-            print "\t\t", key
             
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
