045954fc92d754dfb85da610087183468ede1b8b
Wim Leers <work@wimleers.com>
2009-11-13 14:12:26 +0000

    Use UUID.int instead of hash(uuid) to generate a color.
    git-svn-id: svn://localhost/gs@37 bd4aa7da-f6f4-4997-a956-5ad611f6b780

--- src/DistributedGame/Player.py
+++ src/DistributedGame/Player.py
@@ -8,12 +8,13 @@ class Player(object):
         self.name  = name
 
         # Generate a UUID.
-        self.UUID  = str(uuid.uuid1())
+        u = uuid.uuid1()
+        self.UUID  = str(u)
 
         # Generate a color based on the hash of the UUID.
-        red   = hash(self.UUID) % 255
-        green = hash(self.UUID) / 1000 % 255
-        blue  = hash(self.UUID) / 1000000 % 255
+        red   = u.int % 255
+        green = u.int / 1000 % 255
+        blue  = u.int / 1000000 % 255
         self.color = (red, green, blue)
 
 
